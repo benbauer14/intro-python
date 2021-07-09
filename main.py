f412911918f216ecac434e9a2fe1e569c413f559
Ben Bauer <ben.bauer1@gmail.com>
2021-07-09 15:18:49 -0500

    added len() command

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print("Hello World")
+
+#Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+print('e.g. print("Hello " + "world")')
+print(("New lines can be created with a backslash and n."))
+
+
+print("Hello " +input("What is your name, dawg?"))
+
+name = input("WHat is your name?")
+print(len(name))
